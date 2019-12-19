23c09a3ff029bc676982b41b49bda4e1c8a36eff
Le Mestre <malolemestre29@gmail.com>
2019-12-19 15:57:30 +0100

    configuration de la récupération des données de la BD par le controler

--- src/Controller/ProStagesCController.php
+++ src/Controller/ProStagesCController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Stage;
+use App\Entity\Formation;
+use App\Entity\Entreprise;
 
 class ProStagesCController extends AbstractController
 {
@@ -13,17 +16,40 @@ class ProStagesCController extends AbstractController
      */
     public function index()
     {
-        return $this->render('pro_stages_c/index.html.twig',[
-            'controller_name'=>'ProStagesCController',]);
+      
+
+        // récupérer le répository de l'entité StageFormation
+
+        $repositoryAcceuil = $this->getDoctrine()->getRepository(Stage::class);
+
+        
+        // récuperer les ressources enregistrées en BD
+        
+        $stages = $repositoryAcceuil->findAll();
+
+        // envoyer les ressources récupérées a la vue chargée de les afficher
+
+        return $this->render('pro_stages_c/index.html.twig',['stages' => $stages]);
     }
     
     /**
      * @Route("/entreprises", name="entreprises")
      */
-    public function entreprises()
+    
+        // récupérer le répository de l'entité Entreprise
+
+        $repositoryEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
+        
+        // récuperer les ressources enregistrées en BD
+
+        $entreprises = $repositoryEntreprise->findAll();
+
+        // envoyer les ressources récupérées a la vue chargée de les afficher
+
+        public function entreprises()
     {
-       return $this->render('pro_stages_c/entreprises.html.twig',[
-           'controller_name'=>'ProStageCController',]);
+       return $this->render('pro_stages_c/entreprises.html.twig',['entreprises' => $entreprises $]);
+
     }
 
     /**
@@ -31,25 +57,39 @@ class ProStagesCController extends AbstractController
      */
     public function formations()
     {
-        return $this->render('pro_stages_c/formations.html.twig',[
-            'controller_name'=>'ProStageCController',]);
+        // récupérer le répository de l'entité Formation
+
+        $repositoryFormation = $this->getDoctrine()->getRepository(Formation::class);
+        
+        // récuperer les ressources enregistrées en BD
+
+        $formations = $repositoryFormation->findAll();
+
+        // envoyer les ressources récupérées a la vue chargée de les afficher
+
+        return $this->render('pro_stages_c/formations.html.twig',['formations' => $formations ]);
     }
 
-      /**
-     * @Route("/ressources/{idRessource}", name="ProStage_Ressources")
-     */
-    public function afficher_ressources($idRessource)
-    {
-        return $this->render('pro_stages_c/affichageRessources.html.twig',[
-            'idRessource'=>$idRessource]);
-    }
 
     /**
-     * @Route("/afficher/stage", name="afficher_stage")
+     * @Route("/afficher/stage/{id}", name="afficher_stage")
      */
-    public function afficher_stage()
+    public function afficher_stage($id)
     {
-        return $this->render('pro_stages_c/afficherStage.html.twig',[
-            'controller_name'=>'ProStageCController',]);
+    
+        // récupérer le répository de l'entité Stage
+
+        $repositoryAffStage = $this->getDoctrine()->getRepository(Stage::class);
+        
+        // récuperer les ressources enregistrées en BD
+
+        $ressourcesStages = $repositoryAffStage->findAll();
+
+        // envoyer les ressources récupérées a la vue chargée de les afficher
+
+        return $this->render('pro_stages_c/affichageRessources.html.twig',[
+            'id' => $id, 
+            'ressource' => $ressourcesStages ]);
+
     }
 }
