5cde02bfa48af414fd159a3b297d49d3710371e1
Le Mestre <malolemestre29@gmail.com>
2019-12-19 10:35:45 +0100

    creation des entité : formation, entreprise, stage ainsi que leurs relations

---
+++ src/Entity/Formation.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FormationRepository")
+ */
+class Formation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    private $intitule;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $courteDescription;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Stage", mappedBy="formations")
+     */
+    private $stages;
+
+    public function __construct()
+    {
+        $this->stages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIntitule(): ?string
+    {
+        return $this->intitule;
+    }
+
+    public function setIntitule(string $intitule): self
+    {
+        $this->intitule = $intitule;
+
+        return $this;
+    }
+
+    public function getCourteDescription(): ?string
+    {
+        return $this->courteDescription;
+    }
+
+    public function setCourteDescription(string $courteDescription): self
+    {
+        $this->courteDescription = $courteDescription;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Stage[]
+     */
+    public function getStages(): Collection
+    {
+        return $this->stages;
+    }
+
+    public function addStage(Stage $stage): self
+    {
+        if (!$this->stages->contains($stage)) {
+            $this->stages[] = $stage;
+            $stage->addFormation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStage(Stage $stage): self
+    {
+        if ($this->stages->contains($stage)) {
+            $this->stages->removeElement($stage);
+            $stage->removeFormation($this);
+        }
+
+        return $this;
+    }
+}
